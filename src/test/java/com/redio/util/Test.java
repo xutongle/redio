7b0bffcfe3374ca00b0e95cf9b5ce178b55f5537
KING-XIAOYAO <liyanbo_2794@126.com>
2016-10-24 14:34:30 +0800

    优化

---
+++ src/test/java/com/redio/util/Test.java
@@ -0,0 +1,38 @@
+package com.redio.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by XIAOYAO on 2016/10/24.
+ * jackson转换null测试
+ */
+public class Test {
+
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("a", "b");
+        map.put("c", null);
+
+        String json = mapper.writeValueAsString(map);
+        System.out.println(json);
+
+        User user = new User();
+        user.setId(1);
+        System.out.println(mapper.writeValueAsString(user));
+
+        Map<String, Object> params = new HashMap<>();
+        User u = new User();
+        u.setId(2);
+        params.put("user", u);
+        System.out.println(mapper.writeValueAsString(params));
+    }
+
+}
---
+++ src/test/java/com/redio/util/User.java
@@ -0,0 +1,27 @@
+package com.redio.util;
+
+/**
+ * Created by XIAOYAO on 2016/10/24.
+ */
+public class User {
+
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
